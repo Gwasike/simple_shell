0b3047cf1879e9736a96ad197ef542a77977ce62
Gwasike <wasikegrace177@gmail.com>
2023-11-06 06:48:03 -0800

    0-simpleshell.c

---
+++ 0-simpleshell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+#include <stdio.h>
+
+/**
+ * main - code that passes betty
+ *
+ * Description - description
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Hello, Betty!\n");
+	return (0);
+}
